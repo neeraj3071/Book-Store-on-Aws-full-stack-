cbf42595eda9da79ce538177732c3f359346221e
Neeraj Saini <73292405+neeraj3071@users.noreply.github.com>
2024-12-13 12:08:59 -0500

    Delete bookstore directory

--- bookstore/src/main/java/com/bookshop/bookstore/exception/BookNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.bookshop.bookstore.exception;
-
-public class BookNotFoundException extends RuntimeException{
-
-    public BookNotFoundException(String id){
-        super("Could not found the user by id "+id);
-    }
-}
